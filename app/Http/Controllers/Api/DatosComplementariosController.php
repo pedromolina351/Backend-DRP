19bc74f3fb1212773c1d5c92778a5f64bd0e7c60
Pedro Molina Montoya <pedroalejandromolina90@gmail.com>
2025-01-23 17:34:14 -0600

    Actualizar endpoint de datos complementarios

--- app/Http/Controllers/Api/DatosComplementariosController.php
+++ app/Http/Controllers/Api/DatosComplementariosController.php
@@ -9,7 +9,8 @@ use App\Http\Requests\StoreDatosComplementariosRequest;
 
 class DatosComplementariosController extends Controller
 {
-    public function getDatosComplementariosByPoa($codigo_poa){
+    public function getDatosComplementariosByPoa($codigo_poa)
+    {
         try {
             // Verificar si el codigo_poa existe en la tabla correspondiente
             $poaExists = DB::table('poa_t_poas')->where('codigo_poa', $codigo_poa)->exists();
@@ -44,115 +45,86 @@ class DatosComplementariosController extends Controller
     public function insertDatosComplementarios(StoreDatosComplementariosRequest $request)
     {
         try {
-            // Extraer el primer beneficiario por programa y por pueblo
-            $primerBeneficiarioPrograma = $request->Listado_Beneficiarios_Programa[0];
-            $primerBeneficiarioPueblo = $request->Listado_Beneficiarios_Pueblos[0];
-    
-            // Inserción inicial con el primer beneficiario por programa y pueblo
-            DB::statement('EXEC mmr.sp_Insert_Datos_Complementarios_POA 
+            // Extraer los datos generales de la solicitud
+            $codigoPoa = $request->codigo_poa;
+            $nombreUnidad = $request->NombreUnidad;
+            $responsableUnidad = $request->ResponsableUnidad;
+            $presupuestoTotal = $request->PresupuestoTotal;
+            $inversionMujeres = $request->InversionMujeres;
+            $inversionFamilia = $request->InversionFamilia;
+            $inversionIgualdad = $request->InversionIgualdad;
+            $cantidadTotalBeneficiarios = $request->CantidadTotalBeneficiarios;
+
+            // Actualizar los datos generales (unidad organizativa y presupuesto) con el primer beneficiario por programa y pueblo
+            $primerBeneficiarioPrograma = $request->Listado_Beneficiarios_Programa[0] ?? null;
+            $primerBeneficiarioPueblo = $request->Listado_Beneficiarios_Pueblos[0] ?? null;
+
+            DB::statement('EXEC mmr.sp_Actualizar_Datos_Complementarios_POA 
+            @codigo_poa = :codigo_poa,
+            @GrupoEdadID = :GrupoEdadID,
+            @GeneroID = :GeneroID,
+            @CantidadBeneficiarios = :CantidadBeneficiarios,
+            @PuebloID = :PuebloID,
+            @CantidadPueblo = :CantidadPueblo,
+            @NombreUnidad = :NombreUnidad,
+            @ResponsableUnidad = :ResponsableUnidad,
+            @PresupuestoTotal = :PresupuestoTotal,
+            @InversionMujeres = :InversionMujeres,
+            @InversionFamilia = :InversionFamilia,
+            @InversionIgualdad = :InversionIgualdad,
+            @CantidadTotalBeneficiarios = :CantidadTotalBeneficiarios', [
+                'codigo_poa' => $codigoPoa,
+                'GrupoEdadID' => $primerBeneficiarioPrograma['GrupoEdadID'] ?? null,
+                'GeneroID' => $primerBeneficiarioPrograma['GeneroID'] ?? null,
+                'CantidadBeneficiarios' => $primerBeneficiarioPrograma['CantidadBeneficiarios'] ?? null,
+                'PuebloID' => $primerBeneficiarioPueblo['PuebloID'] ?? null,
+                'CantidadPueblo' => $primerBeneficiarioPueblo['CantidadPueblo'] ?? null,
+                'NombreUnidad' => $nombreUnidad,
+                'ResponsableUnidad' => $responsableUnidad,
+                'PresupuestoTotal' => $presupuestoTotal,
+                'InversionMujeres' => $inversionMujeres,
+                'InversionFamilia' => $inversionFamilia,
+                'InversionIgualdad' => $inversionIgualdad,
+                'CantidadTotalBeneficiarios' => $cantidadTotalBeneficiarios,
+            ]);
+
+            // Iterar sobre los demás beneficiarios por programa
+            foreach (array_slice($request->Listado_Beneficiarios_Programa, 1) as $beneficiarioPrograma) {
+                DB::statement('EXEC mmr.sp_Actualizar_Datos_Complementarios_POA 
                 @codigo_poa = :codigo_poa,
                 @GrupoEdadID = :GrupoEdadID,
                 @GeneroID = :GeneroID,
-                @CantidadBeneficiarios = :CantidadBeneficiarios,
-                @PuebloID = :PuebloID,
-                @CantidadPueblo = :CantidadPueblo,
-                @NombreUnidad = :NombreUnidad,
-                @ResponsableUnidad = :ResponsableUnidad,
-                @PresupuestoTotal = :PresupuestoTotal,
-                @InversionMujeres = :InversionMujeres,
-                @InversionFamilia = :InversionFamilia,
-                @InversionIgualdad = :InversionIgualdad,
-                @CantidadTotalBeneficiarios = :CantidadTotalBeneficiarios', [
-                'codigo_poa' => $request->codigo_poa,
-                'GrupoEdadID' => $primerBeneficiarioPrograma['GrupoEdadID'],
-                'GeneroID' => $primerBeneficiarioPrograma['GeneroID'],
-                'CantidadBeneficiarios' => $primerBeneficiarioPrograma['CantidadBeneficiarios'],
-                'PuebloID' => $primerBeneficiarioPueblo['PuebloID'],
-                'CantidadPueblo' => $primerBeneficiarioPueblo['CantidadPueblo'],
-                'NombreUnidad' => $request->NombreUnidad,
-                'ResponsableUnidad' => $request->ResponsableUnidad,
-                'PresupuestoTotal' => $request->PresupuestoTotal,
-                'InversionMujeres' => $request->InversionMujeres,
-                'InversionFamilia' => $request->InversionFamilia,
-                'InversionIgualdad' => $request->InversionIgualdad,
-                'CantidadTotalBeneficiarios' => $request->CantidadTotalBeneficiarios,
-            ]);
-    
-            // Iterar sobre los demás beneficiarios por programa
-            foreach (array_slice($request->Listado_Beneficiarios_Programa, 1) as $beneficiarioPrograma) {
-                DB::statement('EXEC mmr.sp_Insert_Datos_Complementarios_POA 
-                    @codigo_poa = :codigo_poa,
-                    @GrupoEdadID = :GrupoEdadID,
-                    @GeneroID = :GeneroID,
-                    @CantidadBeneficiarios = :CantidadBeneficiarios,
-                    @PuebloID = NULL,
-                    @CantidadPueblo = NULL,
-                    @NombreUnidad = NULL,
-                    @ResponsableUnidad = NULL,
-                    @PresupuestoTotal = NULL,
-                    @InversionMujeres = NULL,
-                    @InversionFamilia = NULL,
-                    @InversionIgualdad = NULL,
-                    @CantidadTotalBeneficiarios = NULL', [
-                    'codigo_poa' => $request->codigo_poa,
+                @CantidadBeneficiarios = :CantidadBeneficiarios', [
+                    'codigo_poa' => $codigoPoa,
                     'GrupoEdadID' => $beneficiarioPrograma['GrupoEdadID'],
                     'GeneroID' => $beneficiarioPrograma['GeneroID'],
                     'CantidadBeneficiarios' => $beneficiarioPrograma['CantidadBeneficiarios'],
                 ]);
             }
-    
+
             // Iterar sobre los demás beneficiarios por pueblos
             foreach (array_slice($request->Listado_Beneficiarios_Pueblos, 1) as $beneficiarioPueblo) {
-                DB::statement('EXEC mmr.sp_Insert_Datos_Complementarios_POA 
-                    @codigo_poa = :codigo_poa,
-                    @GrupoEdadID = NULL,
-                    @GeneroID = NULL,
-                    @CantidadBeneficiarios = NULL,
-                    @PuebloID = :PuebloID,
-                    @CantidadPueblo = :CantidadPueblo,
-                    @NombreUnidad = NULL,
-                    @ResponsableUnidad = NULL,
-                    @PresupuestoTotal = NULL,
-                    @InversionMujeres = NULL,
-                    @InversionFamilia = NULL,
-                    @InversionIgualdad = NULL,
-                    @CantidadTotalBeneficiarios = NULL', [
-                    'codigo_poa' => $request->codigo_poa,
+                DB::statement('EXEC mmr.sp_Actualizar_Datos_Complementarios_POA 
+                @codigo_poa = :codigo_poa,
+                @PuebloID = :PuebloID,
+                @CantidadPueblo = :CantidadPueblo', [
+                    'codigo_poa' => $codigoPoa,
                     'PuebloID' => $beneficiarioPueblo['PuebloID'],
                     'CantidadPueblo' => $beneficiarioPueblo['CantidadPueblo'],
                 ]);
             }
-    
+
             // Retornar respuesta exitosa
             return response()->json([
                 'success' => true,
-                'message' => 'Datos complementarios insertados con éxito.'
-            ], 201);
-    
+                'message' => 'Datos complementarios actualizados con éxito.'
+            ], 200);
         } catch (\Exception $e) {
             // Manejo de errores
-            $errorMessage = $e->getMessage();
-    
-            if (str_contains($errorMessage, 'Ya existe un beneficiario con el mismo GrupoEdadID y GeneroID para este POA')) {
-                return response()->json([
-                    'success' => false,
-                    'message' => 'El beneficiario con el mismo GrupoEdadID y GeneroID ya está registrado para este POA.',
-                ], 400);
-            }
-    
-            if (str_contains($errorMessage, 'Ya existe un beneficiario del mismo PuebloID para este POA')) {
-                return response()->json([
-                    'success' => false,
-                    'message' => 'El beneficiario del mismo PuebloID ya está registrado para este POA.',
-                ], 400);
-            }
-    
-            // Manejo genérico para otros errores
             return response()->json([
                 'success' => false,
-                'message' => 'Error al insertar los datos complementarios: ' . $errorMessage,
+                'message' => 'Error al actualizar los datos complementarios: ' . $e->getMessage(),
             ], 500);
         }
-    }    
-    
+    }
 }
