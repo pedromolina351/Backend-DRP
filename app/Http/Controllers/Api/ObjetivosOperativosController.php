1f4ce117efe74ffa083c41bec01abb9a3b201ba7
Pedro Molina Montoya <pedroalejandromolina90@gmail.com>
2025-02-09 11:47:53 -0600

    Corregir endpoint objetivos operativos

--- app/Http/Controllers/Api/ObjetivosOperativosController.php
+++ app/Http/Controllers/Api/ObjetivosOperativosController.php
@@ -47,9 +47,11 @@ class ObjetivosOperativosController extends Controller
                     'success' => false,
                     'message' => 'El codigo_poa proporcionado no existe.',
                 ], 400); // Bad Request
+            }else{
+                DB::statement('EXEC [v2].[sp_Delete_t_poa_objetivos_operativos] @codigo_poa = ?', [$request->codigo_poa]);
             }
 
-            DB::statement('EXEC [v2].[sp_Insert_t_objetivos_operativos]
+            $insert = DB::statement('EXEC [v2].[sp_Insert_t_poa_objetivos_operativos]
                 @objetivos_operativos = ?, 
                 @subprogramas_proyectos = ?, 
                 @codigo_poa = ?', [
@@ -58,9 +60,16 @@ class ObjetivosOperativosController extends Controller
                 $request->codigo_poa
             ]);
 
+            if (!$insert) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Error al insertar los objetivos operativos.',
+                ], 500);
+            }
             return response()->json([
                 'success' => true,
                 'message' => 'Objetivos procesados con éxito.',
+                'objetivos_operativos' => $insert,
             ], 201);
         } catch (\Exception $e) {
             return response()->json([
