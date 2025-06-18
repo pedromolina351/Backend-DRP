cddc3a1e42ce08220f9907ace09734a53f5ad7a8
Pedro Molina Montoya <pedroalejandromolina90@gmail.com>
2025-06-17 22:00:45 -0600

    corregir generacion de excel

--- app/Http/Controllers/MatrizPoaController.php
+++ app/Http/Controllers/MatrizPoaController.php
@@ -39,7 +39,6 @@ class MatrizPoaController extends Controller
         $datosProductosIntermedios = $productosIntermediosController->getProductosIntermediosByPoa($codigo_poa);
         $datosActividadInsumo = $actividadInsumoController->getActividadesInsumosByPoaId($codigo_poa);
 
-
         // Extraer JSON de la respuesta de Laravel
         $poaArray = $poaResponse->getData(true);
         $poaDatosArray = $datosPOA->getData(true);
@@ -49,14 +48,6 @@ class MatrizPoaController extends Controller
         $productosFinalesArray = $datosProductosFinales->getData(true);
         $productosIntermediosArray = $datosProductosIntermedios->getData(true);
         $actividadInsumoArray = $datosActividadInsumo->getData(true);
-
-        // Verificar si el JSON tiene éxito y la clave 'data' existe
-        if (!isset($poaArray['success']) || !$poaArray['success'] || !isset($poaArray['data'])) {
-            return response()->json([
-                'success' => false,
-                'message' => 'Error: No se encontraron datos para el poa proporcionado.'
-            ], 400);
-        }
 
         // Extraer los datos del POA
         $dataPOA = $poaArray['data'];
@@ -82,13 +73,13 @@ class MatrizPoaController extends Controller
         }
 
         $politicaRow = 13;
-        foreach ($listadoDatosPoa['Politicas'] as $politica) {
+        foreach ($listadoDatosPoa['Politicas'] ?? [] as $politica) {
             $hoja1->setCellValue($columns[$politicaRow - 13] . $politicaRow, $politica['nombre_politica_publica']);
             $politicaRow++;
         }
 
         $an_odsRow = 15;
-        foreach ($listadoDatosPoa['An_ODs'] as $an_ods) {
+        foreach ($listadoDatosPoa['An_ODs'] ?? [] as $an_ods) {
             $hoja1->setCellValue($columns[$an_odsRow - 15] . $an_odsRow, $an_ods['objetivo_an_ods']);
             $hoja1->setCellValue($columns[$an_odsRow - 15] . ($an_odsRow + 1), $an_ods['meta_an_ods']);
             $hoja1->setCellValue($columns[$an_odsRow - 15] . ($an_odsRow + 2), $an_ods['indicador_an_ods']);
@@ -96,14 +87,14 @@ class MatrizPoaController extends Controller
         }
 
         $vision_paisRow = 19;
-        foreach ($listadoDatosPoa['Vision_Pais'] as $vision_pais) {
+        foreach ($listadoDatosPoa['Vision_Pais'] ?? [] as $vision_pais) {
             $hoja1->setCellValue($columns[$vision_paisRow - 19] . $vision_paisRow, $vision_pais['objetivo_vision_pais']);
             $hoja1->setCellValue($columns[$vision_paisRow - 19] . ($vision_paisRow + 1), $vision_pais['meta_vision_pais']);
             $vision_paisRow++;
         }
 
         $pegRow = 22;
-        foreach ($listadoDatosPoa['PEG'] as $peg) {
+        foreach ($listadoDatosPoa['PEG'] ?? [] as $peg) {
             $hoja1->setCellValue($columns[$pegRow - 22] . $pegRow, $peg['nombre_gabinete']);
             $hoja1->setCellValue($columns[$pegRow - 22] . ($pegRow + 1), $peg['nombre_eje_estrategico']);
             $hoja1->setCellValue($columns[$pegRow - 22] . ($pegRow + 2), $peg['nombre_objetivo_peg']);
@@ -112,70 +103,85 @@ class MatrizPoaController extends Controller
             $pegRow++;
         }
 
-        $impRow = 31;
-        foreach ($impactosArray['impactos'] as $imp) {
-            $hoja1->setCellValue('B' . $impRow, $imp['resultado_final']);
-            $hoja1->setCellValue('C' . $impRow, $imp['indicador_resultado_final']);
-            $impRow += 6;
+        // validar primero el array de impactos
+        if (!empty($impactosArray['impactos'])) {
+            $impRow = 31;
+            foreach ($impactosArray['impactos'] as $imp) {
+                $hoja1->setCellValue('B' . $impRow, $imp['resultado_final']);
+                $hoja1->setCellValue('C' . $impRow, $imp['indicador_resultado_final']);
+                $impRow += 6;
+            }
         }
 
-        $resRow = 31;
-        foreach ($resultadosArray['resultados'] as $res) {
-            $hoja1->setCellValue('D' . $resRow, $res['resultado_institucional']);
-            $hoja1->setCellValue('E' . $resRow, $res['indicador_resultado_institucional']);
-            $resRow += 6;
+        // validar primero el array de resultados
+        if (!empty($resultadosArray['resultados'])) {
+            $resRow = 31;
+            foreach ($resultadosArray['resultados'] as $res) {
+                $hoja1->setCellValue('D' . $resRow, $res['resultado_institucional']);
+                $hoja1->setCellValue('E' . $resRow, $res['indicador_resultado_institucional']);
+                $resRow += 6;
+            }
         }
 
-        $objRow = 31;
-        foreach ($objetivosArray['objetivos'] as $obj) {
-            $hoja1->setCellValue('G' . $objRow, $obj['objetivo_operativo']);
-            $hoja1->setCellValue('H' . $objRow, $obj['subprograma_proyecto']);
-            $objRow += 37;
+        // validar primero el array de objetivos operativos
+        if (!empty($objetivosArray['objetivos'])) {
+            $objRow = 31;
+            foreach ($objetivosArray['objetivos'] as $obj) {
+                $hoja1->setCellValue('G' . $objRow, $obj['objetivo_operativo']);
+                $hoja1->setCellValue('H' . $objRow, $obj['subprograma_proyecto']);
+                $objRow += 37;
+            }
         }
 
-        $prodRow = 31;
-        $contProd = 1;
-        foreach ($productosFinalesArray['productos_finales'] as $prod) {
-            $hoja1->setCellValue('I' . $prodRow, $contProd);
-            $contProd++;
-            $hoja1->setCellValue('J' . $prodRow, $prod['producto_final']);
-            $hoja1->setCellValue('K' . $prodRow, $prod['indicador_producto_final']);
-            $hoja1->setCellValue('M' . $prodRow, $prod['programa']);
-            $hoja1->setCellValue('N' . $prodRow, $prod['subprograma']);
-            $hoja1->setCellValue('O' . $prodRow, $prod['proyecto']);
-            $hoja1->setCellValue('P' . $prodRow, $prod['actividad']);
-            $hoja1->setCellValue('Q' . $prodRow, $prod['costo_total_aproximado']);
-            $hoja1->setCellValue('R' . $prodRow, $prod['nombre_obra']);
-            $prodRow += 1;
+        // validar primero el array de productos finales
+        if (!empty($productosFinalesArray['productos_finales'])) {
+            $prodRow = 31;
+            $contProd = 1;
+            foreach ($productosFinalesArray['productos_finales'] as $prod) {
+                $hoja1->setCellValue('I' . $prodRow, $contProd);
+                $contProd++;
+                $hoja1->setCellValue('J' . $prodRow, $prod['producto_final']);
+                $hoja1->setCellValue('K' . $prodRow, $prod['indicador_producto_final']);
+                $hoja1->setCellValue('M' . $prodRow, $prod['programa']);
+                $hoja1->setCellValue('N' . $prodRow, $prod['subprograma']);
+                $hoja1->setCellValue('O' . $prodRow, $prod['proyecto']);
+                $hoja1->setCellValue('P' . $prodRow, $prod['actividad']);
+                $hoja1->setCellValue('Q' . $prodRow, $prod['costo_total_aproximado']);
+                $hoja1->setCellValue('R' . $prodRow, $prod['nombre_obra']);
+                $prodRow += 1;
+            }
         }
 
-        $prodInterRow = 31;
-        foreach ($productosIntermediosArray['productos_intermedios'] as $prodInter) {
-            $hoja1->setCellValue('T' . $prodInterRow, $prodInter['producto_intermedio']);
-            $hoja1->setCellValue('U' . $prodInterRow, $prodInter['indicador_producto_intermedio']);
-            $hoja1->setCellValue('W' . $prodInterRow, $prodInter['programa']);
-            $hoja1->setCellValue('X' . $prodInterRow, $prodInter['subprograma']);
-            $hoja1->setCellValue('Y' . $prodInterRow, $prodInter['proyecto']);
-            $hoja1->setCellValue('Z' . $prodInterRow, $prodInter['actividad']);
-            $hoja1->setCellValue('AA' . $prodInterRow, $prodInter['fuente_financiamiento']);
-            $hoja1->setCellValue('AB' . $prodInterRow, $prodInter['ente_de_financiamiento']);
-            $hoja1->setCellValue('AC' . $prodInterRow, $prodInter['costro_aproximado']);
-            $prodInterRow += 1;
+        // validar primero el array de productos intermedios
+        if (!empty($productosIntermediosArray['productos_intermedios'])) {
+            $prodInterRow = 31;
+            foreach ($productosIntermediosArray['productos_intermedios'] as $prodInter) {
+                $hoja1->setCellValue('T' . $prodInterRow, $prodInter['producto_intermedio']);
+                $hoja1->setCellValue('U' . $prodInterRow, $prodInter['indicador_producto_intermedio']);
+                $hoja1->setCellValue('W' . $prodInterRow, $prodInter['programa']);
+                $hoja1->setCellValue('X' . $prodInterRow, $prodInter['subprograma']);
+                $hoja1->setCellValue('Y' . $prodInterRow, $prodInter['proyecto']);
+                $hoja1->setCellValue('Z' . $prodInterRow, $prodInter['actividad']);
+                $hoja1->setCellValue('AA' . $prodInterRow, $prodInter['fuente_financiamiento']);
+                $hoja1->setCellValue('AB' . $prodInterRow, $prodInter['ente_de_financiamiento']);
+                $hoja1->setCellValue('AC' . $prodInterRow, $prodInter['costro_aproximado']);
+                $prodInterRow += 1;
+            }
         }
 
-        $actInsumoRow = 31;
-        $contInsumo = 1;
-        foreach ($actividadInsumoArray['actividades_insumos']['ActividadesInsumos'] as $actividad) {
-            $hoja1->setCellValue('AD' . $actInsumoRow, $contInsumo);
-            $contInsumo++;
-            $hoja1->setCellValue('AE' . $actInsumoRow, $actividad['Actividad']);
-            $hoja1->setCellValue('AF' . $actInsumoRow, $actividad['InsumoPACC']);
-            $hoja1->setCellValue('AG' . $actInsumoRow, $actividad['InsumoNoPACC']);
-            $actInsumoRow += 1;
+        // validar primero el array de actividades e insumos
+        if (!empty($actividadInsumoArray['actividades_insumos'])) {
+            $actInsumoRow = 31;
+            $contInsumo = 1;
+            foreach ($actividadInsumoArray['actividades_insumos']['ActividadesInsumos'] as $actividad) {
+                $hoja1->setCellValue('AD' . $actInsumoRow, $contInsumo);
+                $contInsumo++;
+                $hoja1->setCellValue('AE' . $actInsumoRow, $actividad['Actividad']);
+                $hoja1->setCellValue('AF' . $actInsumoRow, $actividad['InsumoPACC']);
+                $hoja1->setCellValue('AG' . $actInsumoRow, $actividad['InsumoNoPACC']);
+                $actInsumoRow += 1;
+            }
         }
-
-
-
 
         // Guardar el archivo temporalmente
         $fileName = 'archivo_excel.xlsx';
@@ -188,7 +194,6 @@ class MatrizPoaController extends Controller
         $writer = IOFactory::createWriter($spreadsheet, 'Xlsx');
         $writer->save($tempPath);
 
-        //return $productosFinalesArray;
         return response()->download($tempPath)->deleteFileAfterSend(true);
     }
 }
